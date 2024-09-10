3c6672398a39c6b3d19f094bf12eec5dd9345df0
Dany0504 <a223206520@unison.mx>
2024-09-10 10:22:30 -0700

    metodo para ordenar la lista usando el metodo de bubuja que vimos en fundamentos

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,12 +4,28 @@ import java.util.List;
 
 public class Main {
 
-
+    //Metodo para combinar las listas
     public static List<Integer> merge(List<Integer> list1, List<Integer> list2) {
         List<Integer> result = new ArrayList<>(list1);
         result.addAll(list2); // Combina las dos listas
         return result;
     }
+
+    //Metodo para ordenar la lista con el algoritmo de burbuja
+    public static void Burbuja(List<Integer> list) {
+        int n = list.size();
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (list.get(j) > list.get(j + 1)) {
+                    int temp = list.get(j);
+                    list.set(j, list.get(j + 1));
+                    list.set(j + 1, temp);
+                }
+            }
+        }
+    }
+
+
 
 
 }
